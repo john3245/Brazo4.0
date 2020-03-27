1cf570eb95dd0d68103c391efc7278122f82672d
john3245 <52746923+john3245@users.noreply.github.com>
2020-03-27 07:49:41 -0600

    Add files via upload

--- brazo4.0.ino
+++ brazo4.0.ino
@@ -1,9 +1,10 @@
 #include <Servo.h>
  
 Servo servoMotor;
-Servo servoMotor2;
+//Servo servoMotor2;
 Servo servoMotor3;
-
+Servo servoMotorpinzagira;
+Servo servoMotorpinza;
 
 
 
@@ -13,20 +14,23 @@ void setup() {
  
   // Iniciamos el servo para que empiece a trabajar con el pin 9
   servoMotor.attach(7);
-    servoMotor2.attach(8);
+    //servoMotor2.attach(8);
      servoMotor3.attach(4);
+      servoMotorpinza.attach(12);
   
   // Inicializamos al ángulo 0 el servomotor
-  servoMotor.write(0);
-  servoMotor2.write(0);
-  servoMotor3.write(0);
+  servoMotor.write(90);
+  //servoMotor2.write(90);
+  servoMotor3.write(90);
+  servoMotorpinza.write(0);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  char opcion='i';
+  char opcion='o';
+  
 
   switch(opcion)
 
@@ -36,18 +40,46 @@ servoMotor3.write(180);
 break;
 
 case 'd':
-servoMotor3.write(0);
+servoMotor3.write(20);
+break;
+
+case 'c':
+servoMotor3.write(90);
 break;
 
 case 'a':
-  servoMotor.write(180);
-  servoMotor2.write(180);
+  servoMotor.write(10);
+  //servoMotor2.write(180);
 
 break;
 
 case 's':
   servoMotor.write( 90);
-  servoMotor2.write(90);
+  //servoMotor2.write(90);
+
+break;
+
+case 'g':
+ servoMotorpinzagira.write(180);
+ 
+
+break;
+
+case 'o':
+ servoMotorpinzagira.write(0);
+ 
+
+break;
+
+case 't':
+ servoMotorpinza.write(180);
+ 
+
+break;
+
+case 'l':
+ servoMotorpinza.write(0);
+ 
 
 break;
     
